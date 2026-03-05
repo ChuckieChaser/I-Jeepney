0115aa5198c1c4e09aafa8d22d6b90aa9b5a11bc
Chuckie <chuckie12602@gmail.com>
2026-03-05 16:15:08 +0800

    signup added

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -2,6 +2,12 @@ const authentication = document.getElementById('authentication');
 const authenticationModalOpen = document.querySelectorAll('.authentication__modal-open');
 const authenticationModalClose = document.getElementById('authenticationModalClose');
 
+const authenticationModalOpenChangeLogin = document.getElementById('authenticationChangeLogin');
+const authenticationModalOpenChangeSignup = document.getElementById('authenticationChangeSignUp');
+
+const loginForm = document.getElementById('loginForm');
+const signupForm = document.getElementById('signupForm');
+
 authenticationModalOpen.forEach((button) => {
     button.addEventListener('click', () => {
         authentication.classList.add('--visible');
@@ -16,4 +22,30 @@ authenticationModalClose.addEventListener('click', () => {
 
     document.body.classList.remove('--modal-open');
     document.documentElement.classList.remove('--modal-open');
+});
+
+authenticationModalOpenChangeLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    signupForm.classList.remove('--visible');
+    loginForm.classList.add('--visible');
+});
+
+authenticationModalOpenChangeSignup.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    loginForm.classList.remove('--visible');
+    signupForm.classList.add('--visible');
+});
+
+const authenticationModalOpenSignUp = document.querySelector('.authentication__modal-open-signup');
+
+authenticationModalOpenSignUp.addEventListener('click', (e) => {
+    loginForm.classList.remove('--visible');
+    signupForm.classList.add('--visible');
+
+    authentication.classList.add('--visible');
+
+    document.body.classList.add('--modal-open');
+    document.documentElement.classList.add('--modal-open');
 });
